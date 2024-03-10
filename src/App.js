934b02831fb019b0d74af8c36adc0f5b3fb2ee85
Digvijay78 <110631494+Digvijay78@users.noreply.github.com>
2024-03-10 21:58:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,90 +1,105 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-import { useEffect, useState } from 'react';
-import Search from './components/Search';
-import NumResult from './components/NumResult';
-import ListBox from './components/ListBox';
-import WatchedBox from './components/WatchedBox';
-import MovieList from './components/MovieList';
-import WatchedSummary from './components/WatchedSummary';
-import WatchedMoviesList from './components/WatchedMoviesList';
-import Loader from './components/Loader';
-import Error from './components/Error';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+import NumResult from "./components/NumResult";
+import ListBox from "./components/ListBox";
+import WatchedBox from "./components/WatchedBox";
+import MovieList from "./components/MovieList";
+import WatchedSummary from "./components/WatchedSummary";
+import WatchedMoviesList from "./components/WatchedMoviesList";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
 
+const KEY = "f84fc31d";
+const tempQuery = "ford";
 
 function App() {
-  const KEY = "f84fc31d";
-  
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState("")
- 
-  const query = "vnvbn";
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-   useEffect(function () {
-     
-     async function fetchingMovies(){
-      setIsLoading(true)
-      
-      try {
 
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-        
-        if(!res.ok){
-          throw new Error("Something went wrong! Kindly check your internet connection");
-        }
+  // useEffect(function () {
+  //   async function fetchingMovies() {
+  //     setIsLoading(true);
 
-        
+  //     try {
+  //       const res = await fetch(
+  //         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+  //       );
+
+  //       if (!res.ok) {
+  //         throw new Error(
+  //           "Something went wrong! Kindly check your internet connection"
+  //         );
+  //       }
+
+  //       const data = await res.json();
+
+  //       if (data.Response === "False") {
+  //         throw new Error("Movie not Found");
+  //       }
+
+  //       setMovies(data.Search);
+  //     } catch (err) {
+  //       // console.log(err.message);
+  //       setError(err.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   fetchingMovies();
+  // }, []);
+
+  useEffect(function () {
+    setIsLoading(true)
+      async function fetchMovies(){
+       try {const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${tempQuery}`)
+
+        if(!res.ok) throw new Error("Something went wrong!")
+
         const data = await res.json();
-        
-        if(data.Response === "False") {
-          throw new Error("Movie not Found")
-          
-          
-        }
-        
-        setMovies(data.Search);
 
+        if(data.Response === 'False') throw new Error("Movie not found :(")
+          
+        setMovies(data.Search)
+        
       } catch(err){
-        // console.log(err.message);
-        setError(err.message);
-        
-
+        setError(err.message)
       } finally{
-        setIsLoading(false)
+          setIsLoading(false)
+
+        }
       }
-      
-    }
-    fetchingMovies()
-  }, [])
-     
-  
+
+      fetchMovies();
+  },[])
+
   return (
     <>
-        <Navbar>
-         <Search />
-         <NumResult movies={movies} />
+      <Navbar>
+        <Search query={query} setQuery={setQuery} />
+        <NumResult movies={movies} />
       </Navbar>
 
       <Main>
-       <ListBox>
-         {/* { isLoading ? <Loader /> : <MovieList movies={movies} />}  */}
+        <ListBox>
          
-         {isLoading &&  <Loader />}
-         {!isLoading && !error && <MovieList movies = {movies} /> }
-         {error && <Error message = {error} /> }
-         </ListBox>
+          {isLoading && <Loader /> }
+          {!isLoading && !error  && <MovieList movies={movies} /> }
+          {error ?  <Error  message ={error} /> :  null}
 
-       <ListBox>
-         <WatchedSummary watched={watched}  />
+        </ListBox>
+
+        <ListBox>
+          <WatchedSummary watched={watched} />
 
           <WatchedMoviesList watched={watched} />
-       </ListBox>
-
-
-         
+        </ListBox>
       </Main>
     </>
   );
