82123670379a465aeffc64d7f945654401599219
Digvijay78 <110631494+Digvijay78@users.noreply.github.com>
2024-03-19 23:20:46 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -18,10 +18,14 @@ const KEY = "f45acb30";
 function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched")
+    return JSON.parse(storedValue)
+  });
 
   function handleSelectedId(id) {
     setSelectedId((prevId) => (prevId === id ? null : id));
@@ -32,12 +36,17 @@ function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
-    setWatched((watched) => watched.filter((movie) => movie.imdbID != id));
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
+  useEffect(() => {
+    localStorage.setItem("watched",  JSON.stringify(watched));
+  },[watched])
   
 
   useEffect(
